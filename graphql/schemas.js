2ac38d9beea7fc566bdc49cd95c5798770adeacb
YERIM <dnaer456@gmail.com>
2020-11-15 01:09:55 +0900

    desc

--- graphql/schemas.js
+++ graphql/schemas.js
@@ -1,5 +1,5 @@
 import path from "path"; // path-> 경로 경로를 추적할 수 있는 것을 impoert함
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from "graphql-tools"; //
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
 const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
