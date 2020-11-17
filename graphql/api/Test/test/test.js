91a382964a394c253dab62ca8ea8e3d68bb4c36c
YERIM <dnaer456@gmail.com>
2020-11-17 23:32:08 +0900

    test graphql

--- graphql/api/Test/test/test.js
+++ graphql/api/Test/test/test.js
@@ -1,17 +1,17 @@
-import Lecture from "../../../model/Lecture"; // Lecture DB를 쓰기 위해 import함
+import Test from "../../../model/Test"; // Test DB를 쓰기 위해 import함
 
 export default {
   // 외부에서 사용하겠다
   Query: {
-    getAllLecture: async (_, args) => {
+    getAllTest: async (_, args) => {
       try {
-        const result = await Lecture.find({}, {});
+        const result = await Test.find({}, {});
         // find({},{특별히 조회할 애를 넣는 곳 넣을 것이 있으면 !})
 
         return result;
       } catch (e) {
         console.log(e);
-        return [];
+        return {};
       }
     },
   },
