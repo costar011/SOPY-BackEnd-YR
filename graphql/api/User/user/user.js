db4a395226d6ecade8f1569554dcbd7213e1a0eb
YERIM <dnaer456@gmail.com>
2021-01-08 23:59:26 +0900

    user

--- graphql/api/User/user/user.js
+++ graphql/api/User/user/user.js
@@ -14,6 +14,30 @@ export default {
         address,
         detailAddress,
       } = args;
+
+      try {
+        const prevResult = await User.find({ email });
+
+        if (prevResult.length !== 0) {
+          console.log("Exist User Email Yet.");
+          return false;
+        } else {
+          const result = await User.create({
+            name,
+            email,
+            nickName: nickName,
+            mobile,
+            zoneCode: zoneCode,
+            address,
+            detailAddress,
+          });
+
+          return true;
+        }
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
     },
   },
 };
