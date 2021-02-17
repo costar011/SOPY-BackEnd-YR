0f6d1b3772b836b79da8fac7986d1a1f752a828d
YERIM <dnaer456@gmail.com>
2021-02-17 22:59:53 +0900

    video CRD

--- graphql/api/Video/video/video.js
+++ graphql/api/Video/video/video.js
@@ -12,5 +12,67 @@ export default {
         return [];
       }
     },
+
+    getVideoOne: async (_, args) => {
+      const { id } = args;
+
+      try {
+        const result = await Video.findOne({ _id: id });
+
+        return result;
+      } catch (e) {
+        console.log(e);
+        return {};
+      }
+    },
+  },
+  Mutation: {
+    registerVideo: async (_, args) => {
+      const { path, title, desc } = args;
+
+      try {
+        await Video.create({
+          thumbnailPath: path,
+          title,
+          description: desc,
+        });
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    deleteVideo: async (_, args) => {
+      const { id } = args;
+
+      try {
+        await Video.deleteOne({ _id: id });
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    updateVideoOne: async (_, args) => {
+      const { id, path, title, desc } = args;
+      try {
+        await Video.updateOne(
+          { _id: id },
+          {
+            $set: {
+              thumbnailPath: path,
+              title,
+              description: desc,
+            },
+          }
+        );
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
   },
 };
